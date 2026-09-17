d1644452e9cfc60c743e53e294b8e90930287bfc
Owen <owen.bridge972@schools.sa.edu.au>
2026-09-17 12:40:14 +0930

    4 buttons

--- sketch.js
+++ sketch.js
@@ -8,18 +8,36 @@ function draw() {
   button.position(100, 100, 100);
   button.size(100, 50);
   button.mousePressed(subjects);
-  let button2 = createButton('other');
+  
+  let button2 = createButton('quiz');
   button2.position(200, 100);
   button2.size(100, 50);
   button2.mousePressed(other);
+  
+  let button3 = createButton('set questions');
+  button3.position(300, 100);
+  button3.size(100, 50);
+  button3.mousePressed(setquestions);
+  
+  let button4 = createButton('other');
+  button4.position(400, 100);
+  button4.size(100, 50);
+  button4.mousePressed(quiz);
 }
 
 function subjects(){
-  console.log ("poop");
+  console.log ("subjects");
 }
 
 function other(){
-  console.log ("goop");
+  console.log ("quiz");
+}
+function setquestions(){
+  console.log ("set questions");
+}
+
+function quiz(){
+  console.log ("other");
 }
 
 //im gooned out
