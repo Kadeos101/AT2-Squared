c5615e3d5d424d77c0f18403606c15788fd695ad
Kadeos101 <kadedbailey@outlook.com>
2026-09-23 13:53:39 +0930

    l

--- subjects.js
+++ subjects.js
@@ -9,20 +9,6 @@ function draw() {
   button.size(100, 50);
   button.mousePressed(backToMain);
   
-  let button2 = createButton('quiz');
-  button2.position(200, 100);
-  button2.size(100, 50);
-  button2.mousePressed(other);
-  
-  let button3 = createButton('set questions');
-  button3.position(300, 100);
-  button3.size(100, 50);
-  button3.mousePressed(setquestions);
-  
-  let button4 = createButton('other');
-  button4.position(400, 100);
-  button4.size(100, 50);
-  button4.mousePressed(quiz);
 }
 
 function backToMain(){
