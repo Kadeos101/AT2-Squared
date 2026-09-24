3e7a36bba15261d278e6b8052872e759971e64a5
Kadeos101 <kadedbailey@outlook.com>
2026-09-24 11:27:52 +0930

    globals

---
+++ Globals.js
@@ -0,0 +1,16 @@
+
+class Question {
+  
+  constructor(x, y, w, h, C) {
+    //Initializes a new Block with position (x, y), width, and height.
+    this.Color = C;
+    this.x = x; // Block's x-coordinate
+    this.y = y; // Block's y-coordinate
+    this.w = w; // Width of the Block
+    this.h = h; // Height of the Block
+  }
+  
+    }
+    rect(this.x, this.y, this.w, this.h); // Draw the wall as a rectangle    
+     
+
