a08b6c9f63b9550e40e19988020480c0e8763cb8
Kadeos101 <kadedbailey@outlook.com>
2026-09-24 12:03:40 +0930

    globals edit

--- Globals.js
+++ Globals.js
@@ -1,16 +1,14 @@
 
 class Question {
   
-  constructor(x, y, w, h, C) {
-    //Initializes a new Block with position (x, y), width, and height.
-    this.Color = C;
-    this.x = x; // Block's x-coordinate
-    this.y = y; // Block's y-coordinate
-    this.w = w; // Width of the Block
-    this.h = h; // Height of the Block
+  constructor(text, answer) {
+    //Initializes a new Question 
+
+    this.text = text; // the question being asked
+    this.answer = answer;
+  
   }
   
     }
-    rect(this.x, this.y, this.w, this.h); // Draw the wall as a rectangle    
-     
 
+  
